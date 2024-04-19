484450e34af02e8bb7d9178ed8230055257314d7
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-19 17:43:02 -0300

    Add project files.

---
+++ TiposTriangulo/Program.cs
@@ -0,0 +1,14 @@
+﻿decimal lado1, lado2, lado3;
+
+do
+{
+    Console.WriteLine("Digite o primeiro lado");
+    lado1 = decimal.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo lado");
+    lado2 = decimal.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o terceiro lado");
+    lado3 = decimal.Parse(Console.ReadLine());
+
+} while (lado1 > 0 && lado2 > 0 && lado3 > 0);
