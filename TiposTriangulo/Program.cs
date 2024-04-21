0b457d8b7fa53a055a31bd4844769dcc86661c33
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-21 15:36:51 -0300

    segundo commit

--- TiposTriangulo/Program.cs
+++ TiposTriangulo/Program.cs
@@ -1,14 +1,41 @@
 ﻿decimal lado1, lado2, lado3;
+int r;
+do {
+    do
+    {
+        Console.WriteLine("Digite o primeiro lado");
+        lado1 = decimal.Parse(Console.ReadLine());
 
-do
-{
-    Console.WriteLine("Digite o primeiro lado");
-    lado1 = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o segundo lado");
+        lado2 = decimal.Parse(Console.ReadLine());
 
-    Console.WriteLine("Digite o segundo lado");
-    lado2 = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o terceiro lado");
+        lado3 = decimal.Parse(Console.ReadLine());
+        if (lado1 == 0 || lado2 == 0 || lado3 == 0)
+        {
+            Console.WriteLine("Lado zerado invalido, repita a declaração");
+        }
 
-    Console.WriteLine("Digite o terceiro lado");
-    lado3 = decimal.Parse(Console.ReadLine());
+    } while (lado1 < 0 || lado2 < 0 || lado3 < 0);
 
-} while (lado1 > 0 && lado2 > 0 && lado3 > 0);
+    if (lado1 == lado2 && lado2 == lado3)
+    {
+        Console.WriteLine("É um triângulo equilátero");
+    }
+    else
+    {
+        if ((lado1 == lado2 && lado2 != lado3) || (lado2 == lado3 && lado3 != lado1) || (lado3 == lado1 && lado1 != lado2))
+        {
+            Console.WriteLine("É um triângulo Isósceles");
+        }
+        else
+        {
+            Console.WriteLine("É um triângulo escaleno");
+        }
+    }
+    do
+    {
+        Console.WriteLine("Deseja digitar outro triângulo? 1 - sim, 2 - não");
+        r = int.Parse(Console.ReadLine());
+    } while (r < 1 && r > 2);
+} while (r == 1);
